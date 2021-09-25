7af738d3db56a28a8048b05216d562d3d1e7b9ca
Susan Babu Pandey <dtmw8z.sp@gmail.com>
2021-09-25 11:04:10 +0100

    firsbase config

--- firebase/config.js
+++ firebase/config.js
@@ -2,23 +2,19 @@
 const firebase = require('firebase/app').default
 import '@firebase/auth';
 import '@firebase/firestore';
-import '@firebase/storage';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDN0gEMIOLIh_TyB8HzyaNXt9-4knFF1TI",
-    authDomain: "student-timetable-8b670.firebaseapp.com",
-    databaseURL: "https://student-timetable-8b670-default-rtdb.firebaseio.com",
-    projectId: "student-timetable-8b670",
-    storageBucket: "student-timetable-8b670.appspot.com",
-    messagingSenderId: "820129933730",
-    appId: "1:820129933730:web:e3aecc8c1a5e3452f4eafb",
-    measurementId: "G-754T7PZJMY"
+    apiKey: "REPLACE_WITH_API_KEY",
+    authDomain: "REPLACE_WITH_AUTH_DOMAIN",
+    projectId: "REPLACE_WITH_PROJECTID",
+    storageBucket: "REPLACE_WITH_STORAGE_BUCKET",
+    messagingSenderId: "REPLACE_WITH_MESSAGING_SENDER_ID",
+    appId: "REPLACE_WITH_APP_ID",
+    measurementId: "REPLACE_WITH_MEASUREMENT_ID"
   };
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
-
-
 
 export { firebase };
